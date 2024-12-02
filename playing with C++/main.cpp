ff4dfa91e3e9410cb1c858be0b96d5b28e501847
ajbla <ajbla@AJ>
2024-12-01 23:10:29 -0600

    Add project files.

---
+++ playing with C++/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//num1 and mum2 store a number.
+	int num1 = 5;
+	int num2 = 4;
+	//calculation calculates num1 - num2 qhich is 1.
+	int calculation = num1 - num2;
+	//this is then printed to the consol using the cout/ consol outpute.
+	cout << calculation << " Wow this is amazing calculations." << endl;
+
+};
